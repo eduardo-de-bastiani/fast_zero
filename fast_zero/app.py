60ba6e090a168e780c033cfe00f1b37df3ce8988
Eduardo De Bastiani <eduardocolladebastiani@gmail.com>
2025-01-05 10:41:30 -0300

    modifying create_user func to use real db

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,12 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI, HTTPException
+from sqlalchemy import create_engine, select, or_
+from sqlalchemy.orm import Session
 
+from fast_zero.models import User
 from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+from fast_zero.settings import Settings
 
 app = FastAPI()
 
@@ -17,13 +21,31 @@ def read_root():
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    user_with_id = UserDB(
-        id=len(database) + 1,
-        **user.model_dump(),  # transforma um objeto em dicionario
-    )
-    database.append(user_with_id)
+    engine = create_engine(Settings().DATABASE_URL)
 
-    return user_with_id
+    with Session(engine) as session:
+        db_user = session.scalar(
+            select(User).where(or_(User.username == user.username, User.email == user.email)
+        ))
+
+        if db_user:
+            if db_user.username == user.username:
+                raise HTTPException(
+                    status_code=HTTPStatus.BAD_REQUEST, detail='Username already exists'
+                )
+
+            elif db_user.email == user.email:
+                raise HTTPException(
+                    status_code=HTTPStatus.BAD_REQUEST, detail='Email already exists'
+                )
+
+        db_user = User(username=user.username, email=user.email, password=user.password)
+
+        session.add(db_user)
+        session.commit()
+        session.refresh(db_user)
+
+    return db_user
 
 
 @app.get('/users/', response_model=UserList)  # nao precisa status code OK (standard)
