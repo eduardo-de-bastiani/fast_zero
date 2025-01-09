bef557b6b95c9931dabcecd8089644064d0f7eac
Eduardo De Bastiani <eduardocolladebastiani@gmail.com>
2025-01-09 14:47:45 -0300

    adding jwt token

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,13 +1,14 @@
 from http import HTTPStatus
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import or_, select
 from sqlalchemy.orm import Session
 
 from fast_zero.database import get_session
 from fast_zero.models import User
-from fast_zero.schemas import Message, UserList, UserPublic, UserSchema
-from fast_zero.security import get_password_hash
+from fast_zero.schemas import Message, UserList, UserPublic, UserSchema, Token
+from fast_zero.security import get_password_hash, verify_password
 
 app = FastAPI()
 
@@ -36,9 +37,10 @@ def create_user(user: UserSchema, session=Depends(get_session)):
             )
 
     db_user = User(
-        username=user.username, 
-        email=user.email, 
-        password= get_password_hash(user.password))
+        username=user.username,
+        email=user.email,
+        password=get_password_hash(user.password),
+    )
 
     session.add(db_user)
     session.commit()
@@ -62,7 +64,7 @@ def update_user(user_id: int, user: UserSchema, session: Session = Depends(get_s
 
     db_user.username = user.username
     db_user.email = user.email
-    db_user.password = user.password
+    db_user.password = get_password_hash(user.password)
 
     session.add(db_user)
     session.commit()
@@ -82,3 +84,18 @@ def delete_user(user_id: int, session: Session = Depends(get_session)):
     session.commit()
 
     return {'message': 'User deleted'}
+
+
+@app.post('/token', response_model=Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session),
+): 
+    user = session.scalar(
+        select(User).where(User.email == form_data.username)
+    )
+    
+    if not user or not verify_password(form_data.password, user.password):
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail='Incorrect email or password')
+
+    
--- fast_zero/security.py
+++ fast_zero/security.py
@@ -1,10 +1,21 @@
 from pwdlib import PasswordHash
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+
+
+from jwt import encode
 
 pwd_context = PasswordHash.recommended()
+
 
 
 def get_password_hash(password: str):
     return pwd_context.hash(password)
 
+
 def verify_password(plain_password: str, hashed_password: str):
     pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_toke(data: dict):
+    
