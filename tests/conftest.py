d5a490a1b23744a87bc35d44ee13f1c6afecb3fc
Eduardo De Bastiani <eduardocolladebastiani@gmail.com>
2025-01-10 18:23:29 -0300

    adding tests for getting token

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ from sqlalchemy.pool import StaticPool
 from fast_zero.app import app
 from fast_zero.database import get_session
 from fast_zero.models import User, table_registry
+from fast_zero.security import get_password_hash
 
 # DRY (don't repeat yourself)
 
@@ -43,10 +44,13 @@ def session():
 # fixture para testes novos usuarios
 @pytest.fixture
 def user(session):
-    user = User(username='teste', email='teste@test.com', password='teste123')
+    pwd = 'senhateste'
+    user = User(username='teste', email='teste@test.com', password=get_password_hash(pwd))
 
     session.add(user)
     session.commit()
     session.refresh(user)
+
+    user.clean_password = pwd  # Monkey Patch
 
     return user
