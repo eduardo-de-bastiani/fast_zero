655876beaf663c9ebec2e5e1bbe4f028f5f9fdf2
Eduardo De Bastiani <eduardocolladebastiani@gmail.com>
2025-01-12 20:29:41 -0300

    adding auth in necessary endpoints

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -8,7 +8,12 @@ from sqlalchemy.orm import Session
 from fast_zero.database import get_session
 from fast_zero.models import User
 from fast_zero.schemas import Message, Token, UserList, UserPublic, UserSchema
-from fast_zero.security import create_access_token, get_password_hash, verify_password, get_current_user
+from fast_zero.security import (
+    create_access_token,
+    get_current_user,
+    get_password_hash,
+    verify_password,
+)
 
 app = FastAPI()
 
@@ -51,40 +56,45 @@ def create_user(user: UserSchema, session=Depends(get_session)):
 
 @app.get('/users/', response_model=UserList)  # nao precisa status code OK (standard)
 def read_users(
-    limit: int = 10, 
-    skip: int = 0, session: Session = Depends(get_session),
-    current_user = Depends(get_current_user)
-    ):
+    limit: int = 10,
+    skip: int = 0,
+    session: Session = Depends(get_session),
+):
     users = session.scalars(select(User).limit(limit).offset(skip))
     return {'users': users}
 
 
 @app.put('/users/{user_id}', response_model=UserPublic)
-def update_user(user_id: int, user: UserSchema, session: Session = Depends(get_session)):
-    db_user = session.scalar(select(User).where(User.id == user_id))
+def update_user(
+    user_id: int,
+    user: UserSchema,
+    session: Session = Depends(get_session),
+    current_user=Depends(get_current_user),
+):
+    if current_user.id != user_id:
+        raise HTTPException(HTTPStatus.BAD_REQUEST, detail='Not enough permissions')
 
-    if not db_user:
-        raise HTTPException(HTTPStatus.NOT_FOUND, detail='User not found')
+    current_user.username = user.username
+    current_user.email = user.email
+    current_user.password = get_password_hash(user.password)
 
-    db_user.username = user.username
-    db_user.email = user.email
-    db_user.password = get_password_hash(user.password)
-
-    session.add(db_user)
+    session.add(current_user)
     session.commit()
-    session.refresh(db_user)
+    session.refresh(current_user)
 
-    return db_user
+    return current_user
 
 
 @app.delete('/users/{user_id}', response_model=Message)
-def delete_user(user_id: int, session: Session = Depends(get_session)):
-    db_user = session.scalar(select(User).where(User.id == user_id))
+def delete_user(
+    user_id: int,
+    session: Session = Depends(get_session),
+    current_user=Depends(get_current_user),
+):
+    if current_user.id != user_id:
+        raise HTTPException(HTTPStatus.BAD_REQUEST, detail='Not enough permissions')
 
-    if not db_user:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='User not found')
-
-    session.delete(db_user)
+    session.delete(current_user)
     session.commit()
 
     return {'message': 'User deleted'}
--- fast_zero/security.py
+++ fast_zero/security.py
@@ -1,12 +1,17 @@
 from datetime import datetime, timedelta
+from http import HTTPStatus
 from zoneinfo import ZoneInfo
-from fast_zero.database import get_session
-from sqlalchemy.orm import Session
-from fastapi import Depends
-from fastapi.security import OAuth2PasswordBearer
 
-from jwt import encode
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jwt import decode, encode
+from jwt.exceptions import PyJWTError
 from pwdlib import PasswordHash
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from fast_zero.database import get_session
+from fast_zero.models import User
 
 pwd_context = PasswordHash.recommended()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
@@ -38,8 +43,29 @@ def create_access_token(data: dict):
 
     return encoded_jwt
 
+
 def get_current_user(
-    session: Session = Depends(get_session),
-    token: str = Depends(oauth2_scheme)
+    session: Session = Depends(get_session), token: str = Depends(oauth2_scheme)
 ):
-    
+    credentials_exception = HTTPException(
+        status_code=HTTPStatus.UNAUTHORIZED,
+        detail='Could not validate credentials',
+        headers={'WWW-Authenticate': 'Bearer'},
+    )
+
+    try:
+        payload = decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        username: str = payload.get('sub')  # username do formulario
+
+        if not username:
+            raise credentials_exception
+
+    except PyJWTError:
+        raise credentials_exception
+
+    user = session.scalar(select(User).where(User.email == username))
+
+    if not user:
+        raise credentials_exception
+
+    return user
