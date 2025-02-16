2499e1d66611fc587e73814293a16553f0f9b1b2
Eduardo De Bastiani <eduardocolladebastiani@gmail.com>
2025-02-16 19:44:10 -0300

    adding CORS

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,11 +1,24 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from fast_zero.routers import auth, tasks, users  # importando os routers
 from fast_zero.schemas import Message
 
 app = FastAPI()
+
+origins = [
+    'http://localhost:5173',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 # incluindo os endpoints
 app.include_router(users.router)
